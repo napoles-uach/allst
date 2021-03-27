4ef6dac0179a40b7096f6af708c8ca11516f415c
José Manuel Nápoles Duarte <jnapoles@uach.mx>
2021-03-26 21:36:55 -0700

    wr

--- allst.py
+++ allst.py
@@ -28,9 +28,9 @@ if b2:
   ngrok.kill()
 #if input_ is not None:
 #  st.write(public_url)
-#  with open('script.sh', 'w') as f:
-#    f.write("#!/bin/bash\n")
-#    f.write("git clone -l -s "+input_ +"\n")
+with open('script.sh', 'w') as f:
+  f.write("#!/bin/bash\n")
+  f.write("git clone -l -s "+input_ +"\n")
 #    f.write("OUTPUT=$(basename " +input_+" .git)\n")
 #    f.write("cd " +""" $OUTPUT """+"\n")
 #    f.write("pip install -r requirements.txt >> outputpip")
